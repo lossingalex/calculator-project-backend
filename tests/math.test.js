edf422c4f98684503802ce1a8a55104cbe1fd181
Alex Lossing <lossing.alex@gmail.com>
2019-09-29 12:31:45 +0800

    test(calculate): fix unit test maths

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,7 +1,7 @@
 const math = require("../libs/math");
 
 test("math basic", () => {
-  const result = math.calculate("12 / (2.3 + 0.7)----");
+  const result = math.calculate("12 / (2.3 + 0.7)");
   expect(result).toEqual(4);
 });
 
