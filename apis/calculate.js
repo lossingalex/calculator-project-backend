f371b7dd4927efef30650fa02240960e6e9534d9
Alex Lossing <lossing.alex@gmail.com>
2019-09-29 18:35:35 +0800

    feature(reporting): refactor reporting inside math calculation

--- apis/calculate.js
+++ apis/calculate.js
@@ -1,40 +1,22 @@
 'use strict';
 const math = require("../libs/math");
-const reporting = require("../libs/reporting");
 const error = require("../libs/error");
 
 module.exports.main = (event, context, callback) => {
   console.log("==== Event.body", event.body);
   // Validation
   if (!event.body) {
-    return error.errorResponse("BAD_REQUEST", "Missing body request");
+    return callback(null, error.errorResponse("BAD_REQUEST", "Missing body request"));
   }
-  const data = JSON.parse(event.body);
+  const requestBody = JSON.parse(event.body);
 
-  if (!data.formula) {
-    return error.errorResponse("BAD_REQUEST", "Missing field: formula");
-  }
-
-  const formula = data.formula;
-
-  // Calculate result
-  try {
-    var result = math.calculate(formula);
-    console.log("=== Result", result);
-  }
-  catch (e) {
-    const message = "Invalid formula:" + formula;
-    // Async call to reporting
-    reporting.addTransaction(formula, message);
-    // Return error message
-    return error.errorResponse("BAD_REQUEST", message);
+  if (!requestBody.formula) {
+    return callback(null, error.errorResponse("BAD_REQUEST", "Missing field: formula"));
   }
 
-  console.log("=== START Transaction");
-  // Async call to reporting
-  reporting.addTransaction(formula, result)
-  .then((data) => {
-    console.log("=== Result After Transaction", data);
+  math.calculateAndLogTransaction(requestBody.formula)
+  .then((result) => {
+    console.log("=== Result After calculateAndLogTransaction", result);
     // Return successfull transaction
     const response = {
       statusCode: 200,
@@ -54,8 +36,8 @@ module.exports.main = (event, context, callback) => {
     return callback(null, response);
   })
   .catch((e) => {
-    console.log("=== Error After Transaction", e);
-    const message = "Internal server Error while trying to add a transaction for:" + formula;
+    console.log("=== Error After calculateAndLogTransaction", e);
+    const message = "====== Internal server Error while trying to add a transaction for:" + requestBody.formula;
     // Return error message
     const response = error.errorResponse("SERVER_ERROR", message);
     return callback(null, response);
